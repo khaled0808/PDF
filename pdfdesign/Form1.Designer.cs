dd71d55ad42366b637ec84701d65240c2b7e1ba8
k.algoursh <k.algoursh@hsp-software.de>
2018-02-19 17:38:14 +0100

    PDF

---
+++ pdfdesign/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace pdfdesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pdfViewerShape = new PdfiumViewer.PdfViewer();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "LiveData";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.LiveData_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Print";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Print_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 48);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "ZoomOut";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ZoomIn_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "SaveAs";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pdfViewerShape
+            // 
+            this.pdfViewerShape.AllowDrop = true;
+            this.pdfViewerShape.AutoSize = true;
+            this.pdfViewerShape.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.pdfViewerShape.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pdfViewerShape.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pdfViewerShape.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pdfViewerShape.Location = new System.Drawing.Point(3, 16);
+            this.pdfViewerShape.Name = "pdfViewerShape";
+            this.pdfViewerShape.ShowToolbar = false;
+            this.pdfViewerShape.Size = new System.Drawing.Size(785, 400);
+            this.pdfViewerShape.TabIndex = 2;
+            this.pdfViewerShape.ZoomMode = PdfiumViewer.PdfViewerZoomMode.FitBest;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 77);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "ZoomIn";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.ZoomOut_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(113, 183);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.pdfViewerShape);
+            this.groupBox2.Location = new System.Drawing.Point(131, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(791, 419);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "PDF Live";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 447);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private PdfiumViewer.PdfViewer pdfViewerShape;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ pdfdesign/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing.Printing;
+using System.IO;
+using System.Text;
+using MigraDoc.Rendering;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Shapes;
+using MigraDoc.DocumentObjectModel.Tables;
+using Patagames.Pdf.Net;
+using PdfiumViewer;
+using System.Runtime.InteropServices;
+namespace pdfdesign
+{
+    public partial class Form1 : Form
+    {
+        int pageNumber;
+        Table table;
+        Section section;
+        string[] rowArray;
+        Paragraph paragraph;
+        private Row row;
+        Column column;
+        TextFrame textFrame;
+        Document document;
+        PdfiumViewer.PdfDocument pdfDocument;
+        MemoryStream pdf = new MemoryStream();
+        public Form1()
+        {
+            InitializeComponent();
+            createPDF(CreateTable("WellCome  TO  HOME"));
+            this.WindowState = FormWindowState.Maximized;
+        }
+       
+        private Document CreateTable(string change)
+        {
+      
+            document = new Document();
+            paragraph = new Paragraph();
+            addTitle(document);
+            showPageNumber();
+            addImage();
+            addTable();
+            column2Width();
+            //string[] rowArray = new string[] { "WellCome  TO  HSP", "WellCome  TO  SBT", change, "WellCome  TO  DHL" };
+            addColumnName();
+            rowArray = new string[] { "A", "B", change, "C" };
+            for (int i = 0; i < 100; i++)
+                addRow(paragraph, table, rowArray);
+            return document;
+        }
+        private void addColumnName()
+        {
+            string[] ColumnName = new string[] { "Column1", "Column2", "Column3", "Column4" };
+            addRow(paragraph, table, ColumnName);
+        }
+
+        private void addTable()
+        {
+            table = section.AddTable();
+            table.Columns.AddColumn();//ADD COLUMNS
+            table.Columns.AddColumn();
+            table.Columns.AddColumn();
+            table.Columns.AddColumn();
+        }
+
+        private void addTitle(Document document)
+        {
+            MigraDoc.DocumentObjectModel.Shapes.TextFrame textFrame = new MigraDoc.DocumentObjectModel.Shapes.TextFrame();
+
+            // Add a section to the document.
+            section = document.AddSection();
+            section.PageSetup.StartingNumber = 1;
+            textFrame = section.AddTextFrame();
+            textFrame.Height = "3.0cm";
+            textFrame.Width = "7.0cm";
+            textFrame.Left = 10;
+            textFrame.Top = Top;
+            // Put sender in address frame
+            paragraph = textFrame.AddParagraph("Handels Software Partner · NotkeStr.9  · 12345 Hamburg");
+        }
+
+        private void addImage()
+        {
+
+            MigraDoc.DocumentObjectModel.Shapes.Image image = section.Headers.Primary.AddImage(@"C:\Users\k.algoursh\Pictures\cat.jpg");
+            image.Height = "1.5cm";
+            image.LockAspectRatio = true;
+            image.RelativeVertical = RelativeVertical.Line;
+            image.RelativeHorizontal = RelativeHorizontal.Margin;
+            image.Top = ShapePosition.Top;
+            image.Left = ShapePosition.Right;
+           image.WrapFormat.Style = WrapStyle.Through;
+        }
+
+        private void column2Width()
+        {
+            IEnumerator columns = table.Columns.GetEnumerator();// wir haben column2 width detected
+            while (columns.MoveNext())
+            {
+                column = (Column)columns.Current;
+                if (column.Index == 2)
+                {
+                    column.Width = 220.0;
+                }
+                else
+                {
+                    column.Width = 80.0;
+                }
+            }
+        }
+
+        private void showPageNumber()
+        {
+       
+            Paragraph pageNumber = new Paragraph();
+            pageNumber.AddText("Page ");
+            pageNumber.AddPageField();
+            pageNumber.AddText(" of ");
+            pageNumber.AddNumPagesField();
+            section.Footers.Primary.Add(pageNumber);
+        }
+
+        private void createPDF(Document document)
+        {
+
+             Path.GetDirectoryName(typeof(int).Assembly.Location);
+            try
+            {
+                // Create a renderer for the MigraDoc document.
+                var pdfRenderer = new PdfDocumentRenderer(false) { Document = document };
+                // Layout and render document to PDF.
+                pdfRenderer.RenderDocument();
+                // Save the document...
+                using (pdf = new MemoryStream())
+                {
+                      pdfRenderer.PdfDocument.Save(pdf);
+                    var pdfReader = new MemoryStream(pdf.GetBuffer());
+                    pdfDocument = PdfiumViewer.PdfDocument.Load(pdfReader);
+
+                  
+                    pdfViewerShape.Renderer.Load(pdfDocument);   // the Problem here cant Found pdfium.dll 
+                    int pagecount = pdfDocument.PageCount;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void addRow(Paragraph paragraph, Table table, String[] rowArray)
+        {
+            row = table.AddRow();
+            addCell(row, 0, rowArray[0]);
+            addCell(row, 1, rowArray[1]);
+            addCell(row, 2, rowArray[2]);
+            addCell(row, 3, rowArray[3]);
+            row.Borders.Color = Colors.White;
+            if (((int)row.Index % 2) == 0)
+            {
+                row.Shading.Color = Colors.Gray;
+            }
+            else
+            {
+                row.Shading.Color = Colors.GreenYellow;
+            }
+            if ((int)row.Index == 0)
+            {
+                row.Shading.Color = Colors.Yellow;
+            }
+        
+        }
+
+        private void addCell(Row row, int index, string text)
+        {
+            textFrame = row.Cells[index].AddTextFrame();
+            textFrame.Width = row.Cells[index].Column.Width;
+            textFrame.Height = 30;
+            textFrame.MarginLeft = 30;
+            textFrame.AddParagraph(text);
+            if ((int)row.Index == 0)
+            {
+                textFrame.MarginLeft = 10;
+            }
+        }
+
+        private void getFocusCurrentPage()
+        {
+            pageNumber = pdfViewerShape.Renderer.Page;
+            createPDF(CreateTable((Guid.NewGuid()).ToString()));
+            int pageCount = pdfViewerShape.Renderer.Document.PageCount;
+            if (pageCount < pageNumber)
+            {
+                pdfViewerShape.Renderer.Page = pageCount;
+            }
+            else
+            {
+                pdfViewerShape.Renderer.Page = pageNumber;
+            }
+        }
+        private void Print_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            var dlg = new PrintDialog();
+            dlg.AllowCurrentPage = true;
+            dlg.AllowSomePages = true;
+            dlg.UseEXDialog = true;
+            dlg.Document = pd;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pdfDocument.CreatePrintDocument();
+            }
+        }
+
+        private void ZoomIn_Click(object sender, EventArgs e)
+        {
+            pdfViewerShape.Renderer.ZoomIn();
+        }
+
+        private void ZoomOut_Click(object sender, EventArgs e)
+        {
+            pdfViewerShape.Renderer.ZoomOut();
+        }
+
+
+        private void LiveData_Click(object sender, EventArgs e)
+        {
+            createPDF(CreateTable(Guid.NewGuid().ToString()));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string test = "test";
+            SaveFileDialog savefile = new SaveFileDialog();
+            string filename = savefile.FileName;
+            savefile.Filter = "(*.pdf)|*.pdf";
+            if (test != null)
+            {
+                if (savefile.ShowDialog() == DialogResult.OK)
+                {
+                    pdfDocument.Save(savefile.FileName);
+                }
+            }
+            else
+            {
+                MessageBox.Show("insert your Data Please");
+            }
+        }
+        //private static bool TryLoadNativeLibrary(string path)
+        //{
+        //    if (path == null)
+        //        return false;
+
+        //    path = Path.Combine(path, IntPtr.Size == 4 ? "x86" : "x64");
+
+        //    path = Path.Combine(path, "pdfium.dll");
+
+        //    return File.Exists(path) && LoadLibrary(path) != IntPtr.Zero;
+        //}
+
+        //[DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
+        //private static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string lpFileName);
+    }
+}
+
